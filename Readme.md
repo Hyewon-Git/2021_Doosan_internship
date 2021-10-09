0bd4debffd2811111637562d0d846a11b8866e5b
Hye_won <khhw2126@gmail.com>
2021-10-09 12:22:28 +0900

    web init

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 ## AWS 환경 구축 > ALB 조회 웹서비스 
+# backend
+# front_end
--- front_end/src/components/Elb.vue
+++ front_end/src/components/Elb.vue
@@ -18,15 +18,13 @@
         name: 'Elb',
         data: () => ({
             headers: [
-                { text: 'ID', value: 'id' },
-                { text: 'Name', value: 'name' },
-                { text: 'Food', value: 'food' },
+                { text: 'Account', value: 'Account' },
+                { text: 'Name', value: 'Name' },
+                { text: 'DNS', value: 'DNS' },
+                { text: 'TYPE', value: 'TYPE' },
+                { text: 'Target Groups State', value: 'Target Groups State' },
             ],
-            contents: [
-                { id: 1, name: 'ABC', food: 'pizza' },
-                { id: 2, name: 'DEF', food: 'chicken' },
-                { id: 3, name: 'GHI', food: 'hamburger' },
-            ]
+            contents: []
             }),
         created() {
             axios.get('http://localhost:5000/elb')
@@ -40,14 +38,14 @@
                     // 오브젝트를 순회하여 key값을 얻어내고
                     // 원하는 데이터만 배열에 다시 밀어넣는다
                     // 이 과정에서 key값은 id 프로퍼티에 추가함!
-                    for(let key in data) {
-                        const elb = data[key]
-                        elb.name = key
-                        datas.push(elb)
-                    }
-                    console.log(datas)
+                    // for(let key in data) {
+                    //     const elb = data[key]
+                    //     elb.name = key
+                    //     datas.push(elb)
+                    // }
+                    // console.log(datas)
                   // 이메일 값만 추출하여 데이터를 업데이트
-                    this.email = datas[0].LoadBalancers
+                    // this.email = datas[0].LoadBalancers
                 })
                 .catch(error => console.log(error))
         }
